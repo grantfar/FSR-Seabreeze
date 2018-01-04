711be32080b05c4d38d83bfdc6eb62732fe79761
grant farnsworth <gzp2307@wmich.edu>
2018-01-04 14:17:21 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/seabreazeDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/seabreazeDemo.dir/main.c.o"
+  "seabreazeDemo.pdb"
+  "seabreazeDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/seabreazeDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <time.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <seabreeze/api/SeaBreezeWrapper.h>
+int main() {
+
+}
