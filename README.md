c833dcbdc99145c6d33e7e1d361b04c4aa17f3c6
James Eckler <james.d.eckler@wmich.edu>
2018-01-09 12:20:28 -0500

    Added documentation to code along with slight edits. Also added a section to the README about setting up -libseabreeze. Also includes compiled code.

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 #set up seabreeze
 sudo cp -R Downloads/seabreeze-3.0.11/SeaBreeze/include/ /usr/local/include/
 sudo mv /usr/local/include/include/ /usr/local/include/seabreeze
+
+#Setting up -libseabreeze
+In the seabreeze-3.0.11/ directory:
+	sudo cp SeaBreeze/lib/libseabreeze.so /usr/lib
+	sudo ldconfig -n -v /usr/lib
--- main.c
+++ main.c
@@ -5,31 +5,30 @@
 #include <time.h>
 #include <sys/time.h>
 #include <errno.h>
-#include <api/SeaBreezeWrapper.h>
+#include "api/SeaBreezeWrapper.h"
 int main() {
     int errorCode;
-    seabreeze_open_spectrometer(0,&errorCode);
+    seabreeze_open_spectrometer(0,&errorCode); //Opens device attached to system
     char model[16];
-    seabreeze_get_model(0,&errorCode,model, sizeof(model));
-    int pixels = seabreeze_get_formatted_spectrum_length(0, &errorCode);
+    seabreeze_get_model(0,&errorCode,model, sizeof(model)); //Returns string denoting type of device
+    int pixels = seabreeze_get_formatted_spectrum_length(0, &errorCode); //Acquires a spectrum and returns answer in formatted floats
     double *spectrum = (double*) malloc(pixels * sizeof(double));
     double *wavelength = (double*) malloc(pixels * sizeof(double));
-    seabreeze_get_wavelengths(0,&errorCode,wavelength,pixels);
+    seabreeze_get_wavelengths(0,&errorCode,wavelength,pixels); //Computes wavelengths for the spectrometer and fills "wavelength" array with those values up to "pixels" value
     spectrum[0]=0;
-    seabreeze_set_integration_time_microsec(0, &errorCode, 10000);
-    seabreeze_get_formatted_spectrum(0,&errorCode,spectrum,pixels);
+    seabreeze_set_integration_time_microsec(0, &errorCode, 10000); //Sets integration time to 10000 microseconds
+    seabreeze_get_formatted_spectrum(0,&errorCode,spectrum,pixels); //Acquires the spectrum and returns the answer in formatted floats and places this in the "spectrum" array up to "pixels" value
 
-    FILE *csv;
-    csv = fopen("spectrum.csv","w");
-
+    FILE *csv = fopen("spectrum.csv","w");
 
     for(int i = 0; i<pixels;){
       for(int k = 0; k < 5 && k+i<pixels; k++){
-        fprintf(csv,"%f,%f\n",wavelength[k+i],spectrum[k+i]);
+        fprintf(csv,"%f,%f\n",wavelength[k+i],spectrum[k+i]); //Writes wavelength and spectrum at each pixel to "spectrum.csv"
       }
       i+=5;
 
     }
     fclose(csv);
+	printf("Output written to \"spectrum.csv\"\n");
     return 0;
 }
